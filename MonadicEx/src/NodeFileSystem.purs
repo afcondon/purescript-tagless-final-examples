21fb2c4199f8fb014568db0f702e6d9735f80eeb
afcondon <andrew.condon@gmail.com>
2017-02-10 19:33:08 +0100

    this is as far as i get ATM
    underlying problem is whether the abstract class should have Eff on the “methods” or whether that can be done somehow at time of evaluation (using a traversal or something???)

--- MonadicEx/src/NodeFileSystem.purs
+++ MonadicEx/src/NodeFileSystem.purs
@@ -1,37 +1,24 @@
 module NodeFileSystem where
 
 import Prelude
-import AbstractFileSystem (class MonadFileSystem, FilePath, FileType)
-import Control.Monad.Aff (launchAff)
+import AbstractFileSystem (class MonadFileSystem, FilePath, FileType(..))
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Class (liftEff)
-import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Monad.Eff.Exception (EXCEPTION)
-import Data.Array (filterM)
-import Data.Maybe (maybe)
 import Data.Profunctor.Strong (first)
-import Data.String (charAt, singleton)
 import Data.Tuple (Tuple(..), fst, snd)
 import Node.FS (FS)
-import Node.FS.Aff (stat, readdir)
-import Node.FS.Stats (isDirectory)
+import Node.FS.Sync (readdir)
 
-main2 :: forall eff. Eff ( err::EXCEPTION, fs::FS, console::CONSOLE| eff) Unit
-main2 = void $ launchAff do
-  files <- readdir "."
-  files' <- flip filterM files \file -> do
-    stat <- stat file
-    pure $ isDirectory stat
-      && (maybe false (singleton >>> (_ /= ".")) $ charAt 0 file)
-  liftEff $ log $ show files'
+data FileSystemState = FileSystemState String
 
-data Cursor = Cursor String
+type FST e a = Eff (fs::FS, err::EXCEPTION|e) a
 
-runFS :: ∀ a. NodeFS a -> Cursor -> a
+data NodeFS a = NodeFS (FileSystemState -> Tuple a FileSystemState)
+
+runFS :: ∀ a. NodeFS a -> FileSystemState -> a
 runFS (NodeFS f) = fst <<< f
 
-data NodeFS a = NodeFS (Cursor -> Tuple a Cursor)
-
+-- | instances for NodeFS
 instance functorNodeFS :: Functor NodeFS where
   map f (NodeFS g) = NodeFS $ first f <<< g
 
@@ -46,20 +33,33 @@ instance bindNodeFS :: Bind NodeFS where
     let ac1 = f c0
     in  (cursor $ k $ fst ac1) (snd ac1)
 
-cursor :: ∀ a. NodeFS a -> (Cursor -> Tuple a Cursor)
+cursor :: ∀ a. NodeFS a -> (FileSystemState -> Tuple a FileSystemState)
 cursor (NodeFS f) = f
 
 instance monadNodeFS :: Monad NodeFS
 
--- readdir :: forall eff. FilePath -> Eff (fs :: FS, err :: EXCEPTION | eff) (Array FilePath)
-
+-- | instance of the underlying abstract class for tagless interpreter
 instance monadfilesystemFakeFS :: MonadFileSystem NodeFS where
   cd _  = pure unit
-  ls    = NodeFS $ ls'
-  cat fs = NodeFS $ cat' fs
+  -- ls    = NodeFS $ ls' -- not compatible with underlying class definition due to Eff
+  ls    = pure []
+  cat fs = pure "not implemented"
 
-ls' :: Cursor -> Tuple (Array (Tuple FilePath FileType)) Cursor
-ls' c@(Cursor s) = Tuple [] c
+ls' :: ∀ e. FileSystemState -> Tuple (FST e (Array (Tuple FilePath FileType))) FileSystemState
+ls' c@(FileSystemState _) = Tuple files c
+    where
+    files :: FST e (Array (Tuple FilePath FileType))
+    files = (map (\s -> Tuple s File)) <$> readdir "."  -- hardwired to "File" for time being
 
-cat' :: Array String -> Cursor -> Tuple String Cursor
-cat' fs c@(Cursor s) = Tuple "file contents" c
+-- cat' :: Array String -> FileSystemState -> Tuple String FileSystemState
+-- cat' fs c@(FileSystemState s) = Tuple "file contents" c
+--
+-- -- | Example main from purescript-node-fs
+-- main2 :: forall eff. Eff ( err::EXCEPTION, fs::FS, console::CONSOLE| eff) Unit
+-- main2 = do
+--   files <- readdir "."
+--   files' <- flip filterM files \file -> do
+--     stat <- stat file
+--     pure $ isDirectory stat
+--       && (maybe false (singleton >>> (_ /= ".")) $ charAt 0 file)
+--   liftEff $ log $ show files'
