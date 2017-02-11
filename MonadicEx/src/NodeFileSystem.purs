7e10ac21585ed3978a50ca791f9856e94e9ec8e1
afcondon <andrew.condon@gmail.com>
2017-02-11 15:08:20 +0100

    take out non-functional FileSystemState

--- MonadicEx/src/NodeFileSystem.purs
+++ MonadicEx/src/NodeFileSystem.purs
@@ -1,25 +1,20 @@
 module NodeFileSystem where
 
 import Prelude
-import AbstractFileSystem (class MonadFileSystem, FilePath, FileType(..))
+import AbstractFileSystem (class MonadFileSystem)
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (logShow)
 import Control.Monad.Eff.Exception (EXCEPTION)
 import Control.Monad.Eff.Unsafe (unsafePerformEff)
-import Data.Profunctor.Strong (first)
-import Data.Tuple (Tuple(..), fst, snd)
 import Node.FS (FS)
 import Node.FS.Sync (readdir)
 
-data FileSystemState = FileSystemState String
-
 newtype FSEff eff a = FSEff (Eff (fs :: FS, err :: EXCEPTION | eff) a)
 
-derive newtype instance functorFSEff     :: Functor (FSEff eff)
-derive newtype instance applyFSEff       :: Apply (FSEff eff)
+derive newtype instance functorFSEff     :: Functor     (FSEff eff)
+derive newtype instance applyFSEff       :: Apply       (FSEff eff)
 derive newtype instance applicativeFSEff :: Applicative (FSEff eff)
-derive newtype instance bindFSEff        :: Bind (FSEff eff)
-derive newtype instance monadFSEff       :: Monad (FSEff eff)
+derive newtype instance bindFSEff        :: Bind        (FSEff eff)
+derive newtype instance monadFSEff       :: Monad       (FSEff eff)
 
 -- gets the Eff out of the FSEff
 runFSEff :: forall eff a. FSEff eff a -> Eff (fs :: FS, err :: EXCEPTION | eff) a
